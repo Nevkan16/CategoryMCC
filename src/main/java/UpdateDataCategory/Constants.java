7d70e331293fa8c3b3a382af4ac757bc99496d05
vladislav_eroshkin <stiff_j@mai.ru>
2024-09-09 22:38:03 +0300

    Constants interface

--- src/main/java/UpdateDataCategory/Constants.java
+++ src/main/java/UpdateDataCategory/Constants.java
@@ -1,14 +1,14 @@
 package UpdateDataCategory;
 
-public class Constants {
-    public static final String ALFA_FILE_PATH = "src/main/resources/MCC_ALFA.txt";
-    public static final String TBANK_FILE_PATH = "src/main/resources/MCC_TBank.txt";
-    public static final String SBER_FILE_PATH = "src/main/resources/MCC_Sber.txt";
-    public static final String VTB_FILE_PATH = "src/main/resources/MCC_VTB.txt";
-    public static final String VTB_URL = "https://mcc-codes.ru/card/vtb-multikarta";
-    public static final String SBER_URL = "https://mcc-codes.ru/card/spasibo-ot-sberbanka";
-    public static final String TBANK_PDF_URL = "https://img-cdn.tinkoffjournal.ru/-/mcc_new_codes.pdf";
-    public static final String TBANK_PDF_TXT = "src/main/Temp/tbankNotFilterPDF.txt";
-    public static final String ALFA_PDF_URL = "https://alfabank.servicecdn.ru/marketing/22/47/marketing/vse_cat.pdf";
-    public static final String ALFA_PDF_TXT = "src/main/Temp/AlfaNotFilterPDF.txt";
+public interface Constants {
+    String ALFA_FILE_PATH = "src/main/resources/MCC_ALFA.txt";
+    String TBANK_FILE_PATH = "src/main/resources/MCC_TBank.txt";
+    String SBER_FILE_PATH = "src/main/resources/MCC_Sber.txt";
+    String VTB_FILE_PATH = "src/main/resources/MCC_VTB.txt";
+    String VTB_URL = "https://mcc-codes.ru/card/vtb-multikarta";
+    String SBER_URL = "https://mcc-codes.ru/card/spasibo-ot-sberbanka";
+    String TBANK_PDF_URL = "https://img-cdn.tinkoffjournal.ru/-/mcc_new_codes.pdf";
+    String TBANK_PDF_TXT = "src/main/Temp/tbankNotFilterPDF.txt";
+    String ALFA_PDF_URL = "https://alfabank.servicecdn.ru/marketing/22/47/marketing/vse_cat.pdf";
+    String ALFA_PDF_TXT = "src/main/Temp/AlfaNotFilterPDF.txt";
 }
